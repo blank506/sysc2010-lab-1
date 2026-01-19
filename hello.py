51522aaaa10d7f43e38cbaa9673965777a397c95
blank506 <ramiohed6@gmail.com>
2026-01-19 13:32:56 -0500

    Update hello.py
    this is where step4.x ends

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print("hello sysc2010")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+print ("libraries are imported successfully!")
+#create a small series
+data = pd.Series([1, 3, 5, np.nan, 6, 8])
+print(data)
+print("hi there")
